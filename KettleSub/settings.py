a4ce5a5cf2115c7f68b2de6b13e6600e9db868e7
Dolapo24 <adedolapovictoria927@gmail.com>
2024-03-07 22:37:13 +0100

    new commit

--- KettleSub/settings.py
+++ KettleSub/settings.py
@@ -159,8 +159,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
