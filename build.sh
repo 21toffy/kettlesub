0bd5aaf3ef35b7491cd571213cabd4082dca6630
Dolapo24 <adedolapovictoria927@gmail.com>
2024-03-07 22:03:18 +0100

    new commit

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@ echo "  BUILD START"
 
 python3 -m venv venv
 source venv/bin/activate
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput --clear
+python manage.py migrate
 
 deactivate
 
