94ee074c6e9e6b4b5a765504cbef0f16ac17731c
Dolapo24 <adedolapovictoria927@gmail.com>
2024-03-08 10:31:44 +0100

    new commit

--- KettleSub/settings.py
+++ KettleSub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8*f=n!qzjorn-5last)0!sl-g@1cbw@07kyxe8%^shqm5^=+kb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kettlesub.onrender.com', 'localhost']
 
 
 # Application definition
