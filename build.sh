2da2e9134e090cf16ac512733d649971d66e06e6
Dolapo24 <adedolapovictoria927@gmail.com>
2024-03-08 09:55:09 +0100

    new commit

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
-echo "  BUILD START"
+set -o errexit
 
-python3 -m pip install --upgrade pip
-python3 -m venv venv
-source venv/bin/activate
+pip install -r requiremennts.txt
 
-pip install --disable-pip-version-check --upgrade -r requirements.txt --config-settings=build_ext --config-settings="-I/usr/include/mysql" --config-settings="-L/usr/lib/mysql" --config-settings="-lmysqlclient"
+python manage.py collectstatic --no-input
 
-python manage.py collectstatic --noinput --clear
-python manage.py migrate
-deactivate
-
-echo "  BUILD END"
+celery -A chrome_extention worker --loglevel=info
