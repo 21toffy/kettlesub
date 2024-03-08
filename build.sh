29c10f7ea285df128b146135d6b69336f164bffe
Dolapo24 <adedolapovictoria927@gmail.com>
2024-03-08 09:25:17 +0100

    new commit

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-
 echo "  BUILD START"
 
 python3 -m pip install --upgrade pip
 python3 -m venv venv
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip install --disable-pip-version-check --upgrade -r requirements.txt --config-settings=build_ext --config-settings="-I/usr/include/mysql" --config-settings="-L/usr/lib/mysql" --config-settings="-lmysqlclient"
 
 python manage.py collectstatic --noinput --clear
 python manage.py migrate
